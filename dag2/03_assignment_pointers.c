dbd261e879151fd4e99a2c97f3218f2b9f80147e
u01u750 <u01u750@cwc.lab>
2025-05-28 08:49:17 +0200

    small pointer assignment

---
+++ dag2/03_assignment_pointers.c
@@ -0,0 +1,26 @@
+// Write a function that asks u
+
+
+
+
+void increment_abc( /* arguments */ )
+{
+	// code
+}
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+	int a = 4;
+	int b = 5;
+	int c = 6;
+
+	increment_abc( /* arguments */ );
+
+	printf("a b c are now: %d %d %d\n",a,b,c);
+
+	return 0;
+}
